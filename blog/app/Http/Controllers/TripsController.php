1a8139e60ef52e901a6ba54cb40c72cc40c3f8b4
10xMike <michelegec@gmail.com>
2021-07-18 19:57:11 +0200

    Add more routes to practice

--- blog/app/Http/Controllers/TripsController.php
+++ blog/app/Http/Controllers/TripsController.php
@@ -8,6 +8,6 @@ class TripsController extends Controller
 {
     public function index(){
         $trips = Trip::all();
-        return view('trips.index', compact(trips));
+        return view('trips.index', compact('trips'));
     }
 }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,10 +11,32 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
-
+ */
+/* Home Route*/
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/trips', 'TripsController@index')->name('trips');
+/* About Route */
+Route::get('about', function () {
+    return view('about');
+});
+/* Products Route */
+Route::get('products', function () {
+    return view('products');
+});
+Route::get('products/{id}', function ($id) {
+    //
+});
+/* Services Route */
+Route::get('services', function () {
+    return view('services');
+});
+/* Users Route */
+Route::get('users', function () {
+    return view('users');
+});
+Route::get('users/{userId}/comments/{commentId}', function ($actualUserId, $actualCommentId) {
+    //
+});
